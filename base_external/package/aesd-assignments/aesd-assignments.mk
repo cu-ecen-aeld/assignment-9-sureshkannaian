c856b6cc3b4eec030a3f0cce9451dae88f0b257e
Suresh Kannaian <suresh.kannaian@colorado.edu>
2022-09-04 01:06:26 -0400

    assignment-4-part2-addl-fix

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 018679b2c5933a275900a2c281aa382b7ea60234
+AESD_ASSIGNMENTS_VERSION = 291653b6cb4c282d0083707637038a953a79fc86
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
