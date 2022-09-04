2a769807e8aca8b78b9639a4f761352827c2f3f6
Suresh Kannaian <suresh.kannaian@colorado.edu>
2022-09-04 13:13:37 -0400

    assignment4-part2 added new comit

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 99ac19ac2dfd40f8712b1e82dccacd0a9ed98910
+AESD_ASSIGNMENTS_VERSION = 329aa260275f10aaccd94c0e728874062da239c9
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
