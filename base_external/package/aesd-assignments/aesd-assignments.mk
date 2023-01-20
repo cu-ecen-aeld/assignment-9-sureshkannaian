527d8a63aa2f96981bb118bb9476b789cc993461
Suresh Kannaian <suresh.kannaian@colorado.edu>
2023-01-20 10:54:21 -0500

    assignment-7 part 2-complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 9ce1943b6b170d2c62809eb584b2089c95dcec09
+AESD_ASSIGNMENTS_VERSION = 792ead463b73e030bb941cf893f9c5d5b8558829
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
